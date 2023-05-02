b82670ade2e71c6874f8293e96e87085ff56006c
Alexandre Arsenault <aarsenault@audiokinetic.com>
2023-05-02 17:07:18 -0400

    update

--- tests/unit_tests/src/array_tests.cpp
+++ tests/unit_tests/src/array_tests.cpp
@@ -6,25 +6,25 @@ namespace
 {
     TEST_CASE("fst::array::push_back()", "[array]")
     {
-        fst::heap_array<float> hppp;
-        //hppp.get_memory_zone();
+        //fst::heap_array<float> hppp;
+        ////hppp.get_memory_zone();
 
-        fst::fixed_heap_array<float, 2> hppp2;
-        //hppp2.get_memory_zone();
-        //fst::array<float, 2> a = {1.0f, 2.0f};
-        //REQUIRE(a[0] == 1.0f);
-        //fst::array<int, 2> a = { 1, 2 };
-        fst::array<float, 8> array;
-        fst::fixed_heap_array<float, 32> fharray;
+        //fst::fixed_heap_array<float, 2> hppp2;
+        ////hppp2.get_memory_zone();
+        ////fst::array<float, 2> a = {1.0f, 2.0f};
+        ////REQUIRE(a[0] == 1.0f);
+        ////fst::array<int, 2> a = { 1, 2 };
+        //fst::array<float, 8> array;
+        //fst::fixed_heap_array<float, 32> fharray;
 
-        fst::heap_array<float> harray;
-        REQUIRE(harray.data() == nullptr);
-        REQUIRE(harray.size() == 0);
+        //fst::heap_array<float> harray;
+        //REQUIRE(harray.data() == nullptr);
+        //REQUIRE(harray.size() == 0);
 
-        fst::fixed_array_range<float, 8> rarray(array);
-        REQUIRE(rarray.size() == 8);
+        //fst::fixed_array_range<float, 8> rarray(array);
+        //REQUIRE(rarray.size() == 8);
 
-        fst::array_range<float> rrarray(array);
-        REQUIRE(rrarray.size() == 8);
+        //fst::array_range<float> rrarray(array);
+        //REQUIRE(rrarray.size() == 8);
     }
 } // namespace
